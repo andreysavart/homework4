7208cdcc433dadfa8aa521c8709ca5d8a11df763
andreysavart <andrejsavkin751@gmail.com>
2024-05-30 16:33:24 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+#homework4
+my_string = input("Введите строку: ")
+print("Длина строки:", len(my_string))
+print("Строка в верхнем регистре:", my_string.upper())
+print("Строка в нижнем регистре:", my_string.lower())
+my_string = my_string.replace(" ", "")
+print("Первый символ строки:", my_string[0])
+print("Последний символ строки:", my_string[-1])
